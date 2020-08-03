a4b0cfb283613ce69327ac3a7261c52ccc833fd5
Avutzhan Dautov <dautov92@list.ru>
2020-08-03 13:50:03 +0600

    Styling the Project Page

---
+++ resources/views/projects/card.blade.php
@@ -0,0 +1,8 @@
+
+    <div class="card" style="height: 200px">
+        <h3 class="text-xl py-4 -ml-5 mb-3 border-l-4 border-blue-300 pl-4">
+            <a href="{{ $project->path() }}">{{ $project->title }}</a>
+        </h3>
+
+        <div class="text-gray-500">{{  str_limit($project->description, 50)  }}</div>
+    </div>
