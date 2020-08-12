ae6282a9b3527387818b1d50c160060383c05b98
Avutzhan Dautov <dautov92@list.ru>
2020-08-12 12:21:57 +0600

    Easy Tailwind Themes Using CSS Variables

--- resources/views/projects/card.blade.php
+++ resources/views/projects/card.blade.php
@@ -1,9 +1,9 @@
     <div class="card mb-4 flex-col" style="height: 200px">
-        <h3 class="text-xl py-4 -ml-5 mb-3 border-l-4 border-blue-300 pl-4">
+        <h3 class="text-xl text-default py-4 -ml-5 mb-3 border-l-4 border-blue-300 pl-4">
             <a href="{{ $project->path() }}">{{ $project->title }}</a>
         </h3>
 
-        <div class="text-gray-500 mb-4 flex-1">{{  str_limit($project->description, 50)  }}</div>
+        <div class="text-default mb-4 flex-1">{{  str_limit($project->description, 50)  }}</div>
 
         @can('manage', $project)
             <footer>
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <header class="flex items-center mb-3 py-4 px-3">
         <div class="flex items-end justify-between w-full">
-            <p class="text-gray-500">
+            <p class="text-default">
                 <a href="/projects">My Projects</a> / {{ $project->title }}
             </p>
 
@@ -22,7 +22,7 @@
         <div class="lg:flex">
             <div class="lg:w-3/4 px-3 mb-6">
                 <div class="mb-8">
-                    <h2 class="text-lg text-gray-500 mb-3">Tasks</h2>
+                    <h2 class="text-lg text-default mb-3">Tasks</h2>
 
                     @foreach($project->tasks as $task)
 
@@ -33,7 +33,7 @@
                                 @csrf
 
                                 <div class="flex">
-                                    <input type="text" value="{{ $task->body }}" name="body" class="w-full {{ $task->completed ? 'text-gray-500' : '' }}">
+                                    <input type="text" value="{{ $task->body }}" name="body" class="bg-card text-default w-full {{ $task->completed ? 'text-gray-500' : '' }}">
                                     <input type="checkbox" name="completed" onchange="this.form.submit()" {{ $task->completed ? 'checked' : '' }}>
                                 </div>
 
@@ -44,13 +44,13 @@
                     <div class="card mb-3">
                         <form action="{{ $project->path() . '/tasks' }}" method="POST">
                             @csrf
-                            <input type="text" placeholder="Add a new task..." class="w-full" name="body">
+                            <input type="text" placeholder="Add a new task..." class="bg-card text-default w-full" name="body">
                         </form>
                     </div>
                 </div>
 
                 <div>
-                    <h2 class="text-lg text-gray-500 mb-3">General Notes</h2>
+                    <h2 class="text-lg text-default mb-3">General Notes</h2>
 
                     <form action="{{ $project->path() }}" method="POST">
                         @csrf
